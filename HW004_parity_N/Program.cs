4824ea72e516f6dc8245fa267b5ae453c3e7a841
Kostya <optics76@yandex.ru>
2023-03-09 14:50:51 +0300

    add HomeWork 004 - Task 8 - parity N

---
+++ HW004_parity_N/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+// 5  -> 2, 4
+// 8  -> 2, 4, 6, 8
+Console.Clear();
+Console.Write("Введите положительное число число N: ");
+int N = int.Parse(Console.ReadLine()!);
+int index=1;
+Console.Write($"{N} - > ");
+while (index<=N)
+{
+    if (index%2==0) 
+    {
+        if (index!=2) Console.Write($", ");
+        Console.Write($"{index}");
+    }
+    index++;
+}

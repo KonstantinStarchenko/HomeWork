9f82181adf5dcc06db4b7561f144f8d85f6b9970
Kostya <optics76@yandex.ru>
2023-03-13 12:14:01 +0300

    edit HomeWork6 Task 13 add numeric method from teacher

--- HW006_Num_13/Program.cs
+++ HW006_Num_13/Program.cs
@@ -16,3 +16,39 @@ else
 {
     Console.WriteLine($"{NumStr} -> третьей цифры нет");
 }
+
+
+//Числовой метод
+int Prompt(string message)
+{
+    Console.Write(message);
+    String value = Console.ReadLine();
+    int result = Convert.ToInt32(value);
+    return result;
+}
+
+int GetThirdRank(int number)
+{
+    while (number > 999)
+    {
+        number /= 10;
+//        Console.WriteLine(number);
+    }
+    return number % 10;
+}
+
+bool ValidateNumber (int number)
+{
+    if (number < 100)
+    {
+        Console.WriteLine("Третьей цифры нет");
+        return false;
+    }
+    return true;
+}
+
+int number = Prompt("Введите число > ");
+if (ValidateNumber(number))
+{
+    Console.WriteLine(GetThirdRank(number));
+}
